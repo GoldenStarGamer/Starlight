e1f01e04be080a36120f437135921dffe00ddcee
Starman <75682000+GoldenStarGamer@users.noreply.github.com>
2024-06-23 17:07:35 +0100

    ok

--- Starlight/Game.cs
+++ Starlight/Game.cs
@@ -9,22 +9,70 @@ namespace Starlight
     {
         private static List<Entity> ents = [];
 
+        float[] vertices = 
+        {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            0.5f, -0.5f, 0.0f, //Bottom-right vertex
+            0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+        int VertexBufferObject;
+
+        Shader? shader;
+
         public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings()
         {
             ClientSize = (width, height), Title = title
         }) { }
 
-        public override void Run()
+        protected override void OnLoad()
         {
+            base.OnLoad();
             KeyRun esctoquit = new(this, Keys.Escape, () => Close());
             ents.Add(esctoquit);
-            base.Run();
+            GL.ClearColor(0.2f, 0.3f, 0.3f,1.0f);
+            VertexBufferObject = GL.GenBuffer();
+
+            shader = new("shaders\\vertex.vert", "shaders\\fragment.frag");
+            //shader.Use();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            SwapBuffers();
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs args)
         { 
             base.OnUpdateFrame(args);
             Entity.MegaUpdate(ents);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            int VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            if(shader != null)
+            shader.Use();
+        }
+
+        protected override void OnUnload()
+        {
+            if(shader != null)
+            shader.Dispose();
+            base.OnUnload();
         }
     }
 }
--- Starlight/Program.cs
+++ Starlight/Program.cs
@@ -4,7 +4,7 @@
 	{
 		static void Main(string[] args)
 		{
-			Game game = new(600, 800, "Starlight");
+			Game game = new(800, 600, "Starlight");
 			game.Run();
 		}
 	}
