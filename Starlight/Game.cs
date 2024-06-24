1ccce4b58162a6974f730f1d42e0758f7d1452b5
Starman <75682000+GoldenStarGamer@users.noreply.github.com>
2024-06-24 22:42:04 +0100

    multi colored square

--- Starlight/Game.cs
+++ Starlight/Game.cs
@@ -14,11 +14,13 @@ namespace Starlight
              // positions        // colors
              0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
             -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-             0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+             0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   // top right
+            -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.5f,   // top left
         };
 
         uint[] indices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
+            0, 1, 2, // first triangle
+            1, 2, 3,
         };
 
         int VertexBufferObject;
